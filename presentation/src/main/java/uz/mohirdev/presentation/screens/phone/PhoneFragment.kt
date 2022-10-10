b12670f5dd4320e9c7dcb26445d3c29218cdc3bf
imnadev <imnadev@gmail.com>
2022-10-10 23:42:36 +0500

    4. MVI

--- presentation/src/main/java/uz/mohirdev/presentation/screens/phone/PhoneFragment.kt
+++ presentation/src/main/java/uz/mohirdev/presentation/screens/phone/PhoneFragment.kt
@@ -1,7 +1,29 @@
 package uz.mohirdev.presentation.screens.phone
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import uz.mohirdev.domain.model.User
+import uz.mohirdev.presentation.base.BaseFragment
+import uz.mohirdev.presentation.databinding.FragmentPhoneBinding
+import uz.mohirdev.presentation.screens.phone.PhoneViewModel.Effect
 
-class PhoneFragment : Fragment() {
+class PhoneFragment : BaseFragment<FragmentPhoneBinding>(FragmentPhoneBinding::inflate) {
 
+    private lateinit var viewModel : PhoneViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.state.observe(::renderUser) { it.user }
+        viewModel.effects.doOnNext(::handleEffects)
+    }
+
+    private fun renderUser(user: User?) {
+
+    }
+
+    private fun handleEffects(effect: Effect) {
+
+    }
 }
--- presentation/src/main/java/uz/mohirdev/presentation/screens/phone/PhoneViewModel.kt
+++ presentation/src/main/java/uz/mohirdev/presentation/screens/phone/PhoneViewModel.kt
@@ -1,10 +1,25 @@
 package uz.mohirdev.presentation.screens.phone
 
-import androidx.lifecycle.ViewModel
+import uz.mohirdev.domain.model.User
 import uz.mohirdev.domain.usecase.auth.SendSmsCodeUseCase
+import uz.mohirdev.presentation.base.BaseViewModel
+import uz.mohirdev.presentation.screens.phone.PhoneViewModel.*
 
 class PhoneViewModel constructor(
     private val sendSmsCodeUseCase: SendSmsCodeUseCase
-) : ViewModel() {
+) : BaseViewModel<State, Input, Effect>() {
 
+    data class State(
+        val user: User? = null
+    )
+
+    class Effect
+
+    class Input
+
+    override fun getDefaultState() = State()
+
+    override fun processInput(input: Input) {
+
+    }
 }
